d71d82ca3e472ad25c3066239d7f28093dd80a34
campbellcompton <campbellcompton@yahoo.com>
2016-07-25 21:02:32 -0400

    Filtering stopwords out of title, ready to vectorize!

--- news.py
+++ news.py
@@ -1,5 +1,6 @@
 import feedparser
 import json
+from nltk.corpus import stopwords
 from utilities import strip_punctuation
 
 
@@ -7,6 +8,7 @@ class News:
 
     def __init__(self):
         self.articles = []
+        self.stopwords = stopwords.words("english")
 
     def get_articles(self):
         with open('feeds.json') as feeds_file:
@@ -16,8 +18,9 @@ class News:
             parsed_feed = feedparser.parse(feed['url'])
 
             for ent in parsed_feed.entries:
+                sanitized_title = strip_punctuation(ent.title)
                 self.articles.append({'raw_title': ent.title,
-                                      'sanitized_title': strip_punctuation(ent.title),
+                                      'cleaned_title': ' '.join(
+                                          [word for word in sanitized_title.split() if word not in self.stopwords]),
                                       'link': ent.link})
-        # TODO remove "stop words" (blacklist) before sending back
         return self.articles

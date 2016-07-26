c3ddfa557ff9769c831095ed313704bbf9304497
Daniel Garrick <danieljamesgarrick@gmail.com>
2016-07-26 02:08:41 -0500

    beginning news source multithreading

--- src/news.py
+++ src/news.py
@@ -2,6 +2,8 @@ import json
 from timeit import default_timer as timer
 import feedparser
 from nltk.corpus import stopwords
+import Queue
+import threading
 
 from utilities import strip_punctuation
 
@@ -10,23 +12,22 @@ class News:
 
     def __init__(self):
         self.articles = []
+        self.article_queue = Queue.Queue()
         self.stopwords = stopwords.words("english")
+
+    def get_feed(self, feed):
+        print feed
 
     def get_articles(self):
         with open('resources/feeds.json') as feeds_file:
             feeds = json.load(feeds_file)
 
         for feed in feeds['feeds']:
-            start = timer()
-            parsed_feed = feedparser.parse(feed['url'])
-            end = timer()
-            print(str(len(parsed_feed.entries)) + " Articles from '" + feed['name'] +
-                  "' in " + str((end-start)) + " seconds")
-            for ent in parsed_feed.entries:
-                sanitized_title = strip_punctuation(ent.title)
-                self.articles.append({'raw_title': ent.title,
-                                      'cleaned_title': ' '.join(
-                                          [word for word in sanitized_title.split() if word not in self.stopwords]),
-                                      'source': feed['name'],
-                                      'link': ent.link})
+            print feed
+            t = threading.Thread(target=self.get_feed, args="hi")
+            t.daemon = True
+            t.start()
+
+        while self.article_queue.not_empty:
+            self.articles.append(self.article_queue.get())
         return self.articles

d8cb2eebd93c8b3356a1e2071b1b98bfa9a4b9c3
Daniel Garrick <danieljamesgarrick@gmail.com>
2016-07-26 23:08:33 -0500

    multiprocessing for news feeds

--- src/news.py
+++ src/news.py
@@ -3,7 +3,7 @@ from timeit import default_timer as timer
 import feedparser
 from nltk.corpus import stopwords
 import Queue
-import threading
+from multiprocessing.dummy import Pool
 
 from utilities import strip_punctuation
 
@@ -16,18 +16,25 @@ class News:
         self.stopwords = stopwords.words("english")
 
     def get_feed(self, feed):
-        print feed
+        start = timer()
+        parsed_feed = feedparser.parse(feed['url'])
+        end = timer()
+        print(str(len(parsed_feed.entries)) + " Articles from '" + feed['name'] +
+              "' in " + str((end - start)) + " seconds")
+        for ent in parsed_feed.entries:
+            sanitized_title = strip_punctuation(ent.title)
+            self.article_queue.put({'raw_title': ent.title,
+                                  'cleaned_title': ' '.join(
+                                      [word for word in sanitized_title.split() if word not in self.stopwords]),
+                                  'source': feed['name'],
+                                  'link': ent.link})
 
     def get_articles(self):
-        with open('resources/feeds.json') as feeds_file:
+        with open('../resources/feeds.json') as feeds_file:
             feeds = json.load(feeds_file)
+        pool = Pool(len(feeds))
+        pool.map(self.get_feed, feeds['feeds'])
 
-        for feed in feeds['feeds']:
-            print feed
-            t = threading.Thread(target=self.get_feed, args="hi")
-            t.daemon = True
-            t.start()
-
-        while self.article_queue.not_empty:
+        while not self.article_queue.empty:
             self.articles.append(self.article_queue.get())
         return self.articles

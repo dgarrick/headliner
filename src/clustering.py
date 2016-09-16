6bd4502590c2603182b37d04f41a3f3b98e2304e
Daniel Garrick <danieljamesgarrick@gmail.com>
2016-09-16 01:21:28 -0500

    little more progress. First pass at a rough hierarchical ranking algorithm for headlines.

--- src/clustering.py
+++ src/clustering.py
@@ -1,4 +1,5 @@
 from annoy import AnnoyIndex
+import numpy
 import bisect
 
 class Clustering:
@@ -6,20 +7,29 @@ class Clustering:
     def __init__(self, vecs):
         self.vec_length = len(vecs[0])
         self.vecs = vecs
-        self.headlines = []
+        self.ranks = 10
+        self.dists = numpy.zeros(self.ranks)
+        self.labels = []
         self.annoy = AnnoyIndex(self.vec_length, metric='euclidean')
         for i, vec in enumerate(self.vecs):
             self.annoy.add_item(i, vec)
         print("Built an index with " + str(self.annoy.get_n_items()) + " headlines")
         self.annoy.build(10)
-        #self.rank_headlines()
+        self.rank_headlines()
 
     def get_neighbors_vector(self, vec):
         return self.annoy.get_nns_by_vector(vec, 10, include_distances=True)
 
-    """def rank_headlines(self):
+    def rank_headlines(self):
         for vec in self.vecs:
-            if len(self.headlines) > 0:
-                bisect.insort_right(self.headlines,(vec,self.get_neighbors_vector(vec)),0,len(self.headlines))
-            else:
-                self.headlines.append((vec,self.get_neighbors_vector(vec)))   """ 
+            total = 0
+            closest_arr = self.get_neighbors_vector(vec)[1]
+            for dist in closest_arr:
+                total += dist * 10
+            #shits not working
+            index = numpy.searchsorted(self.dists, total)
+            print(index)
+            if index < self.ranks:
+                self.dists = numpy.insert(self.dists, index, total)
+                print(self.dists[index])
+            self.labels.insert(index, vec)

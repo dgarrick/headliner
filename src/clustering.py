bb2fd2579656e22af28e198e12c09d2060c4c4e6
Daniel Garrick <danieljamesgarrick@gmail.com>
2016-09-17 21:28:15 -0500

    added a rough popularity ranking for vectors. Realized we need to cluster them first, and then run the popularity ranking on vectors in each cluster. Then we can order the vectors in a cluster by density, and likewise for the clusters themselves.This should give us a good 'trending stories' list.

--- src/clustering.py
+++ src/clustering.py
@@ -7,29 +7,45 @@ class Clustering:
     def __init__(self, vecs):
         self.vec_length = len(vecs[0])
         self.vecs = vecs
-        self.ranks = 10
-        self.dists = numpy.zeros(self.ranks)
-        self.labels = []
         self.annoy = AnnoyIndex(self.vec_length, metric='euclidean')
         for i, vec in enumerate(self.vecs):
             self.annoy.add_item(i, vec)
         print("Built an index with " + str(self.annoy.get_n_items()) + " headlines")
         self.annoy.build(10)
-        self.rank_headlines()
 
     def get_neighbors_vector(self, vec):
         return self.annoy.get_nns_by_vector(vec, 10, include_distances=True)
 
-    def rank_headlines(self):
+    def get_vector_index(self, vec):
+        return self.annoy.get_nns_by_vector(vec, 1, include_distances=False)
+
+    @staticmethod
+    def find_sorted_index(dists, total):
+        """Returns None if no index can be found"""
+        for i, dist in enumerate(dists):
+            if total > dist:
+                return i
+
+    def rank_vectors(self, num_ranks):
+        """Finds the top N vectors which have the most dense clustering to other vectors.
+           Returns a list of vectors zipped with their respective density score"""
+        dists = numpy.zeros(num_ranks)
+        labels = [None] * num_ranks
         for vec in self.vecs:
             total = 0
             closest_arr = self.get_neighbors_vector(vec)[1]
             for dist in closest_arr:
-                total += dist * 10
-            #shits not working
-            index = numpy.searchsorted(self.dists, total)
-            print(index)
-            if index < self.ranks:
-                self.dists = numpy.insert(self.dists, index, total)
-                print(self.dists[index])
-            self.labels.insert(index, vec)
+                if dist > 0:
+                    total += 1.0 / dist
+            index = 0
+            local_vec = vec
+            while index is not None:
+                index = self.find_sorted_index(dists, total)
+                if index is not None:
+                    tmp_total = dists[index]
+                    tmp_vec = labels[index]
+                    numpy.put(dists, index, total)
+                    labels[index] = local_vec
+                    total = tmp_total
+                    local_vec = tmp_vec
+        return zip(labels, dists)

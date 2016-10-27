f83f65804276b234346285ac330e3110528d1ad2
Daniel Garrick <danieljamesgarrick@gmail.com>
2016-10-26 23:51:31 -0500

    initial front-end check in. Also added scheduling library to requirements.txt

--- headliner.py
+++ headliner.py
@@ -1,5 +1,7 @@
 import falcon
+from src.api.staticfile import StaticFile
 from src.api.clusters import Clusters
 
 api = falcon.API()
+api.add_route('/', StaticFile("src/static/index.html"))
 api.add_route('/clusters', Clusters('src/resources/clusters.json'))
